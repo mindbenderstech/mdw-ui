7ddd5ded21147a1ec1068eba1fdc6c0ab52fa4e6
pratikaherMBT <pratikaher98@gmail.com>
2025-05-29 19:08:11 +0530

    upadted url for conection to backend

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 
 // Function to fetch all articles
 export const getAllArticles = async () => {
