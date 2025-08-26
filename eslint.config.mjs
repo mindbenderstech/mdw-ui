9e30bfdbd22ee5db59b176a3d959a9b5b57ae316
pratikaherMBT <pratikaher98@gmail.com>
2025-08-26 22:04:38 +0530

    updated rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
   {
     rules: {
       '@next/next/no-img-element': 'off',  // Disable the no-img-element rule
+      '@next/next/no-danger' : 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
     },
   },
 ];
